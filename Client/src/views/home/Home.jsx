0ef5a5a9af7ef3ba4f8e112fed149dc84e79a6cb
Aman kumar <87026675+Aman300@users.noreply.github.com>
2024-06-01 13:52:22 +0500

    update

--- Client/src/views/home/Home.jsx
+++ Client/src/views/home/Home.jsx
@@ -123,7 +123,7 @@ async function fetchOpenGame(){
 
 
 
-    <div className='flex justify-start ml-5'>
+    <div className='flex justify-start ml-5 mt-10 xl:mt-0'>
         <div onClick={() => setShowModal(true)} className=' cursor-pointer py-3 px-5 bg-rose-50 rounded-2xl text-rose-500 font-semibold flex gap-1'>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
@@ -134,8 +134,8 @@ async function fetchOpenGame(){
         {showModal ? (
         <>
           <div className="xl:ml-[300px] flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-            <div className="relative w-1/2 h-[400px]">
-              <div className="border-0 rounded-xl shadow-xl relative flex flex-col w-full bg-white outline-none focus:outline-none p-6">
+            <div className="relative xl:w-1/2 h-[400px]">
+              <div className="border-0 rounded-xl shadow-xl relative flex flex-col w-full bg-white outline-none focus:outline-none xl:p-6 p-5">
               <div className='flex justify-between ' onClick={() => setShowModal(false)}>
                   <p className=' text-bold font-semibold'>Amount can only accept 50 to 25000</p>
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 cursor-pointer hover:text-rose-600">
@@ -200,22 +200,22 @@ async function fetchOpenGame(){
 
       
         {data &&  data.map((item, index)=>(
-               <div className='flex justify-around items-center border rounded-xl py-4 mt-2'>
+               <div className='flex justify-around items-center border rounded-xl xl:py-4 py-1 mt-2'>
                <div className=' flex justify-around items-center gap-2'>
-                 <img className='size-12' src={item.user_id.profile} alt="" />
+                 <img className='xl:size-12 size-10' src={item.user_id.profile} alt="" />
                  <p>{item.user_id.name}</p>
                </div>
                <div className='text-center'>
-                 <img className='size-10' src="https://static.vecteezy.com/system/resources/previews/022/949/509/non_2x/vs-versus-letters-logo-icon-isolated-on-white-background-vs-versus-symbol-for-confrontation-or-opposition-design-concept-vector.jpg" alt="" />
+                 <img className='xl:size-10 size-8' src="https://static.vecteezy.com/system/resources/previews/022/949/509/non_2x/vs-versus-letters-logo-icon-isolated-on-white-background-vs-versus-symbol-for-confrontation-or-opposition-design-concept-vector.jpg" alt="" />
                  <p>{item.game_amount}</p>
                </div>
                <div className='text-center flex justify-center items-center'>            
                {
-                item.user_id._id === userId._id ? (<button onClick={() => deleteGame(item._id)} className='text-center py-2 px-5 bg-rose-600 hover:bg-rose-700 text-white font-semibold rounded-xl'>
+                item.user_id._id === userId._id ? (<button onClick={() => deleteGame(item._id)} className='text-center xl:py-2 py-1 xl:px-5 p-3 bg-rose-600 hover:bg-rose-700 text-white font-semibold rounded-xl'>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                   <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                 </svg>    
-                </button>)  : (<button className='text-center py-2 px-5 bg-yellow-400 hover:bg-yellow-500 text-white font-semibold rounded-xl'>
+                </button>)  : (<button className='text-center xl:py-2 py-1 xl:px-5 p-3 bg-yellow-400 hover:bg-yellow-500 text-white font-semibold rounded-xl'>
                   Play
                 </button>)
                }
